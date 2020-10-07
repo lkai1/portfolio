1fe880e031ecd3821924ddb0fc5c4b6b94ca9e4c
lkai1 <kai-le@outlook.com>
2020-10-08 02:47:17 +0300

    portfolio
    Started building portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="navBar">
+        <span className="navGrid">
+          <p>empty</p>
+          <p>PORTFOLIO</p>
+          <p id="contanctBtn">Contact</p>
+        </span>
+      </div>
+      <div className="wrapper">
+        <div id="leftFront">
+          <span id="nameText">
+          <h2>KAI LEHTINEN</h2>
+          <p>FrontEnd/FullStack Web developer</p>
+          </span>
+        </div>
+        <div id="rightFront">
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
